4f822a820b9d5ddcf70ba75adfdb542f4672e84f
Younis <23105954+Younis-Ahmed@users.noreply.github.com>
2024-03-11 19:27:43 +0000

    added argparse

--- cli.py
+++ cli.py
@@ -1,5 +1,5 @@
 from argparse import ArgumentParser, Namespace
 
-parser = ArgumentParser(description='A simple CLI')
+parser = ArgumentParser(description='SeniorSWE cli tool utilize AI to help you with your project')
 
 parser.add_argument('init', help='initialize the project')
