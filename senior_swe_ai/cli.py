4b6483f504cbdc280c63e744cbe887a4f965da74
Younis <23105954+Younis-Ahmed@users.noreply.github.com>
2024-03-16 21:54:07 +0000

    Add git repository information to configuration file

--- senior_swe_ai/cli.py
+++ senior_swe_ai/cli.py
@@ -1,7 +1,9 @@
 """ SeniorSWE cli tool utilize AI to help you with your project """
 from argparse import ArgumentParser, Namespace
 import sys
-from senior_swe_ai.conf import config_init
+from typing import Any
+from senior_swe_ai.git_process import is_git_repo, get_repo_name, get_repo_root
+from senior_swe_ai.conf import config_init, load_conf, append_conf
 
 
 def main() -> None:
@@ -21,6 +23,22 @@ def main() -> None:
         print('Initializing the app...')
         config_init()
         sys.exit()
+
+    if not is_git_repo():
+        print('The current directory is not a git repository')
+        sys.exit(1)
+
+    repo_name: str = get_repo_name()
+    repo_root: str = get_repo_root()
+
+    append_conf({'repo_name': repo_name, 'repo_root': repo_root})
+
+    try:
+        conf: dict[Any, Any] = load_conf()
+    except FileNotFoundError:
+        config_init()
+        append_conf({'repo_name': repo_name, 'repo_root': repo_root})
+        conf = load_conf()
 
 
 if __name__ == '__main__':
