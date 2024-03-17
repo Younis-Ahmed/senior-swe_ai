7c8f75ab730afea800ca5abb61ff32602c78d2fd
Younis <23105954+Younis-Ahmed@users.noreply.github.com>
2024-03-17 23:02:53 +0000

    Add recursive_load_files function to load all files in the git repository

--- senior_swe_ai/git_process.py
+++ senior_swe_ai/git_process.py
@@ -1,5 +1,7 @@
 """     This module contains functions to interact with git repositories. """
+import os
 import subprocess
+from senior_swe_ai.consts import EXCLUDE_DIRS, EXCLUDE_FILES, INCLUDE_FILES
 
 
 def is_git_repo() -> bool:
@@ -23,3 +25,20 @@ def get_repo_name() -> str:
     return subprocess.run(
         ["git", "rev-parse", "--show-toplevel"], capture_output=True, check=True, text=True
     ).stdout.strip().split('/')[-1]
+
+
+def recursive_load_files() -> list[str]:
+    """ Load all files in the git repository """
+    git_root: str = get_repo_root()
+    file_list: list = []
+
+    for root, _, files in os.walk(git_root):
+        if any(blacklist in root for blacklist in EXCLUDE_DIRS):
+            continue
+        for file in files:
+            file_ext: str = os.path.splitext(file)[1]
+            if any(whitelist == file_ext for whitelist in INCLUDE_FILES):
+                if file not in EXCLUDE_FILES:
+                    file_list.append(os.path.join(root, file))
+
+    return file_list
