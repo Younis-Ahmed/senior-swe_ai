7831b255e2546ef721b94f6ec7b3f6a17a3a64ba
Younis <23105954+Younis-Ahmed@users.noreply.github.com>
2024-03-19 21:02:21 +0000

    Add language and text splitting functionality

--- senior_swe_ai/cli.py
+++ senior_swe_ai/cli.py
@@ -2,9 +2,12 @@
 from argparse import ArgumentParser, Namespace
 import os
 import sys
+from typing import List
+from langchain_core.documents.base import Document
 from langchain_openai import OpenAIEmbeddings
 from langchain_community.document_loaders.generic import GenericLoader
 from langchain_community.document_loaders.parsers.language.language_parser import LanguageParser
+from langchain_text_splitters import Language, RecursiveCharacterTextSplitter
 from senior_swe_ai.file_handler import get_extension
 from senior_swe_ai.git_process import (
     is_git_repo, get_repo_name, get_repo_root, recursive_load_files
@@ -61,17 +64,23 @@ def main() -> None:
     if not os.path.exists(get_cache_path() + f'/{repo_name}.faiss'):
         # all desired files in the git repository tree
         files: list[str] = recursive_load_files()
-        # ext_arr = []
-        for file in files:
-            ext: str = get_extension(file)
-            # ext_arr.append(ext)
-            loader: GenericLoader = GenericLoader.from_filesystem(
-                file,
-                glob="*/[!.]*",
-                suffixes=ext,
-                parser=LanguageParser(
-                    language=get_langchain_text_splitters(ext))
-            )
+
+        exts: set = {get_extension(file) for file in files}
+        loader: GenericLoader = GenericLoader.from_filesystem(
+            repo_root,
+            glob="*/[!.]*",
+            suffixes=list(exts),
+            parser=LanguageParser()
+        )
+        docs: List[Document] = loader.load()
+
+        text_splitter: RecursiveCharacterTextSplitter = RecursiveCharacterTextSplitter(
+            chunk_size=1000,
+            chunk_overlap=100,
+
+        )
+        text: List[Document] = text_splitter.split_documents(docs)
+
 
 
 if __name__ == '__main__':
