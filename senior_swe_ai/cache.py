54cef33ab4f4dcd7b017f45476356c142b1df90b
Younis <23105954+Younis-Ahmed@users.noreply.github.com>
2024-03-21 21:12:30 +0000

    Add VectorCache class and related functions to cache vector data

--- senior_swe_ai/cache.py
+++ senior_swe_ai/cache.py
@@ -1,9 +1,38 @@
 """ This module contains functions to create and get the cache directory
 for the application.
 """
+import json
 import os
 import platform
 from pathlib import Path
+
+from altair import Self
+
+
+class VectorCache:
+    """ VectorCache class for storing vector data """
+
+    def __init__(self, filename, vec_ids, commit_hash) -> None:
+        self.filename = filename
+        self.vector_ids = vec_ids
+        self.commit_hash = commit_hash
+
+    def to_json(self) -> str:
+        """Convert the object to json"""
+        return {
+            "filename": self.filename,
+            "commit_hash": self.commit_hash,
+            "vector_ids": self.vector_ids,
+        }
+
+    @classmethod
+    def from_json(cls, data: dict[str, str]) -> Self:
+        """Create a VectorCache object from json"""
+        return cls(
+            filename=data.get("filename"),
+            vec_ids=data.get("vector_ids"),
+            commit_hash=data.get("commit_hash"),
+        )
 
 
 def get_cache_path() -> str:
@@ -28,3 +57,22 @@ def create_cache_dir() -> None:
     if not os.path.exists(get_cache_path()):
         path = Path(get_cache_path())
         path.mkdir(parents=True, exist_ok=True)
+
+
+def load_vec_cache(filename: str) -> dict[str, VectorCache]:
+    """Load the vector cache from the given file."""
+    with open(get_cache_path() + f'/{filename}', 'r', encoding='utf-8') as f:
+        vec = json.load(f)
+    vec_cache = {}
+    for key, value in vec.items():
+        vec_cache[key] = VectorCache.from_json(value)
+    return vec_cache
+
+
+def save_vec_cache(vector_cache, filename) -> None:
+    """Save the vector cache to the given file."""
+    with open(
+        get_cache_path() + "/" + filename, "w", encoding="utf-8"
+    ) as vector_cache_file:
+        json.dump(vector_cache, default=VectorCache.to_json,
+                  fp=vector_cache_file)
