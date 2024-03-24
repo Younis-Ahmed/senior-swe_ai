60466fd65f99fa891d42c46302719bbf6830f607
Younis <23105954+Younis-Ahmed@users.noreply.github.com>
2024-03-24 18:31:27 +0000

    Refactor C++ tree parser implementation

--- senior_swe_ai/tree_parser/tree_parser_cpp.py
+++ senior_swe_ai/tree_parser/tree_parser_cpp.py
@@ -1,21 +1,25 @@
 """Module to parse C++ code using the tree-sitter library."""
+import warnings
 import tree_sitter
 from senior_swe_ai.consts import Language
-from senior_swe_ai.tree_parser.base import Treesitter
-from senior_swe_ai.tree_parser.tree_parser_registry import TreesitterRegistry
+from senior_swe_ai.tree_parser.base import BaseTreeParser
+from senior_swe_ai.tree_parser.tree_parser_registry import TreeParserRegistry
 
 
-class TreesitterCpp(Treesitter):
+class TreeParserCpp(BaseTreeParser):
     """Class to parse C++ code using the tree-sitter library."""
-    def __init__(self):
-        super().__init__(Language.CPP, "function_definition", "identifier", "comment")
 
-    def _query_method_name(self, node: tree_sitter.Node):
+    def __init__(self) -> None:
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            super().__init__(Language.CPP, "function_definition", "identifier", "comment")
+
+    def _query_method_name(self, node: tree_sitter.Node) -> str | None:
         if node.type == self.method_declaration_identifier:
             for child in node.children:
                 # if method returns pointer, skip pointer declarator
                 if child.type == "pointer_declarator":
-                    child = child.children[1]
+                    child: tree_sitter.Node = child.children[1]
                 if child.type == "function_declarator":
                     for child in child.children:
                         if child.type == self.method_name_identifier:
@@ -23,4 +27,4 @@ class TreesitterCpp(Treesitter):
         return None
 
 
-TreesitterRegistry.register_treesitter(Language.CPP, TreesitterCpp)
+TreeParserRegistry.register_treesitter(Language.CPP, TreeParserCpp)
