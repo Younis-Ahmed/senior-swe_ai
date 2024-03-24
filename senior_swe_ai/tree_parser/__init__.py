d83ca04d527c07cb16e74b68573dd85f69719b30
Younis <23105954+Younis-Ahmed@users.noreply.github.com>
2024-03-24 18:31:00 +0000

    Update tree parser imports

--- senior_swe_ai/tree_parser/__init__.py
+++ senior_swe_ai/tree_parser/__init__.py
@@ -1,13 +1,13 @@
 """ This module is responsible for parsing the ASTs of different programming languages. """
-from senior_swe_ai.tree_parser.base import Treesitter, TreesitterMethodNode
-from senior_swe_ai.tree_parser.tree_parser_c import TreesitterC
-from senior_swe_ai.tree_parser.tree_parser_cpp import TreesitterCpp
-from senior_swe_ai.tree_parser.tree_parser_cs import TreesitterCsharp
-from senior_swe_ai.tree_parser.tree_parser_go import TreesitterGo
-from senior_swe_ai.tree_parser.tree_parser_java import TreesitterJava
-from senior_swe_ai.tree_parser.tree_parser_js import TreesitterJavascript
-from senior_swe_ai.tree_parser.tree_parser_kt import TreesitterKotlin
-from senior_swe_ai.tree_parser.tree_parser_py import TreesitterPython
-from senior_swe_ai.tree_parser.tree_parser_rb import TreesitterRuby
-from senior_swe_ai.tree_parser.tree_parser_rs import TreesitterRust
-from senior_swe_ai.tree_parser.tree_parser_ts import TreesitterTypescript
+from senior_swe_ai.tree_parser.base import BaseTreeParser, TreeParserMethodNode
+from senior_swe_ai.tree_parser.tree_parser_c import TreeParserC
+from senior_swe_ai.tree_parser.tree_parser_cpp import TreeParserCpp
+from senior_swe_ai.tree_parser.tree_parser_cs import TreeParserCsharp
+from senior_swe_ai.tree_parser.tree_parser_go import TreeParserGo
+from senior_swe_ai.tree_parser.tree_parser_java import TreeParserJava
+from senior_swe_ai.tree_parser.tree_parser_js import TreeParserJs
+from senior_swe_ai.tree_parser.tree_parser_kt import TreeParserKotlin
+from senior_swe_ai.tree_parser.tree_parser_py import TreeParsePy
+from senior_swe_ai.tree_parser.tree_parser_rb import TreeParseRuby
+from senior_swe_ai.tree_parser.tree_parser_rs import TreeParseRust
+from senior_swe_ai.tree_parser.tree_parser_ts import TreeParseTypescript
