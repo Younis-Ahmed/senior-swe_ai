4e603ffebec74b474fe3556df72377c915ac861f
Younis <23105954+Younis-Ahmed@users.noreply.github.com>
2024-03-30 22:18:02 +0000

    Add type hinting and docstrings to TreeParserRegistry class

--- senior_swe_ai/tree_parser/tree_parser_registry.py
+++ senior_swe_ai/tree_parser/tree_parser_registry.py
@@ -1,4 +1,5 @@
 """Registry for tree-sitter parsers."""
+from typing import Any
 from senior_swe_ai.consts import Language
 
 
@@ -8,13 +9,26 @@ class TreeParserRegistry:
 
     @classmethod
     def register_treesitter(cls, name, treesitter_class) -> None:
-        """Register a tree-sitter parser."""
+        """
+        Register a tree-sitter parser.
+
+        Args:
+            name (Language): The language of the tree-sitter parser.
+            treesitter_class (BaseTreeParser): The tree-sitter parser class.
+
+        """
         cls._registry[name] = treesitter_class
 
     @classmethod
-    def create_treesitter(cls, name: Language):
-        """Factory method to create a tree-sitter parser for the given language."""
-        treesitter_class = cls._registry.get(name)
+    def create_treesitter(cls, name: Language) -> Any:
+        """
+        Factory method to create a tree-sitter parser for the given language.
+
+        Args:
+            name (Language): The language of the tree-sitter parser.
+
+        """
+        treesitter_class: Any | None = cls._registry.get(name)
         if treesitter_class:
             return treesitter_class()
 
